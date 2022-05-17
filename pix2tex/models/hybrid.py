6c531057f4232750cd6607ddd6cfcba5f5673f54
Lukas Blecher <luk.blecher@gmail.com>
2022-05-17 16:50:37 +0200

    move transformer decoder to its own file

--- pix2tex/models/hybrid.py
+++ pix2tex/models/hybrid.py
@@ -1,62 +1,11 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
-from x_transformers.autoregressive_wrapper import AutoregressiveWrapper, top_k, top_p, entmax, ENTMAX_ALPHA
 from timm.models.vision_transformer import VisionTransformer
 from timm.models.vision_transformer_hybrid import HybridEmbed
 from timm.models.resnetv2 import ResNetV2
 from timm.models.layers import StdConv2dSame
-from einops import rearrange, repeat
-
-
-class CustomARWrapper(AutoregressiveWrapper):
-    def __init__(self, *args, **kwargs):
-        super(CustomARWrapper, self).__init__(*args, **kwargs)
-
-    @torch.no_grad()
-    def generate(self, start_tokens, seq_len=256, eos_token=None, temperature=1., filter_logits_fn=top_k, filter_thres=0.9, **kwargs):
-        device = start_tokens.device
-        was_training = self.net.training
-        num_dims = len(start_tokens.shape)
-
-        if num_dims == 1:
-            start_tokens = start_tokens[None, :]
-
-        b, t = start_tokens.shape
-
-        self.net.eval()
-        out = start_tokens
-        mask = kwargs.pop('mask', None)
-        if mask is None:
-            mask = torch.full_like(out, True, dtype=torch.bool, device=out.device)
-
-        for _ in range(seq_len):
-            x = out[:, -self.max_seq_len:]
-            mask = mask[:, -self.max_seq_len:]
-            # print('arw:',out.shape)
-            logits = self.net(x, mask=mask, **kwargs)[:, -1, :]
-
-            if filter_logits_fn in {top_k, top_p}:
-                filtered_logits = filter_logits_fn(logits, thres=filter_thres)
-                probs = F.softmax(filtered_logits / temperature, dim=-1)
-
-            sample = torch.multinomial(probs, 1)
-
-            out = torch.cat((out, sample), dim=-1)
-            mask = F.pad(mask, (0, 1), value=True)
-
-            if eos_token is not None and (torch.cumsum(out == eos_token, 1)[:, -1] >= 1).all():
-                break
-
-        out = out[:, t:]
-
-        if num_dims == 1:
-            out = out.squeeze(0)
-
-        self.net.train(was_training)
-        return out
-
+from einops import repeat
 
 class CustomVisionTransformer(VisionTransformer):
     def __init__(self, img_size=224, patch_size=16, *args, **kwargs):
@@ -84,28 +33,7 @@ class CustomVisionTransformer(VisionTransformer):
         return x
 
 
-class Model(nn.Module):
-    """ViT encoder, transformer decoder architecture"""
-
-    def __init__(self, encoder: CustomVisionTransformer, decoder: CustomARWrapper, args, temp: float = .333):
-        super().__init__()
-        self.encoder = encoder
-        self.decoder = decoder
-        self.bos_token = args.bos_token
-        self.eos_token = args.eos_token
-        self.max_seq_len = args.max_seq_len
-        self.temperature = temp
-
-    @torch.no_grad()
-    def forward(self, x: torch.Tensor):
-        device = x.device
-        encoded = self.encoder(x.to(device))
-        dec = self.decoder.generate(torch.LongTensor([self.bos_token]*len(x))[:, None].to(device), self.max_seq_len,
-                                    eos_token=self.eos_token, context=encoded, temperature=self.temperature)
-        return dec
-
-
-def get_encoder(args, training=False):
+def get_encoder(args):
     backbone = ResNetV2(
         layers=args.backbone_layers, num_classes=0, global_pool='', in_chans=args.channels,
         preact=False, stem_type='same', conv_layer=StdConv2dSame)
--- pix2tex/models/utils.py
+++ pix2tex/models/utils.py
@@ -1,22 +1,21 @@
 import torch
 import torch.nn as nn
 
-from x_transformers import TransformerWrapper, Encoder, Decoder
-from x_transformers.autoregressive_wrapper import AutoregressiveWrapper
-
 from . import hybrid
 from . import vit
+from . import transformer
 
 
 class Model(nn.Module):
-    def __init__(self, encoder: Encoder, decoder: AutoregressiveWrapper, args):
+    def __init__(self, encoder, decoder, args):
         super().__init__()
         self.encoder = encoder
         self.decoder = decoder
         self.args = args
 
     def forward(self, x: torch.Tensor):
-        return self.decoder.generate(torch.LongTensor([self.args.bos_token]*len(x)).to(x.device), self.args.max_seq_len, eos_token=self.args.eos_token, context=self.encoder(x))
+        return self.decoder.generate(torch.LongTensor([self.args.bos_token]*len(x)).to(x.device),
+                                     self.args.max_seq_len, eos_token=self.args.eos_token, context=self.encoder(x))
 
 
 def get_model(args, training=False):
@@ -26,20 +25,9 @@ def get_model(args, training=False):
         encoder = hybrid.get_encoder(args)
     else:
         raise NotImplementedError('Encoder structure "%s" not supported.' % args.encoder_structure)
-    decoder = AutoregressiveWrapper(
-        TransformerWrapper(
-            num_tokens=args.num_tokens,
-            max_seq_len=args.max_seq_len,
-            attn_layers=Decoder(
-                dim=args.dim,
-                depth=args.num_layers,
-                heads=args.heads,
-                cross_attend=True
-            )),
-        pad_value=args.pad_token
-    )
-    available_gpus = torch.cuda.device_count()
-    if available_gpus > 1:
+    decoder = transformer.get_decoder(args)
+    num_available_gpus = torch.cuda.device_count()
+    if num_available_gpus > 1:
         encoder = nn.DataParallel(encoder)
         decoder = nn.DataParallel(decoder)
     encoder.to(args.device)
@@ -54,12 +42,10 @@ def get_model(args, training=False):
     model = Model(encoder, decoder, args)
     if training:
         # check if largest batch can be handled by system
-        batchsize = args.batchsize if args.get(
-            'micro_batchsize', -1) == -1 else args.micro_batchsize
+        batchsize = args.batchsize if args.get('micro_batchsize', -1) == -1 else args.micro_batchsize
         im = torch.empty(batchsize, args.channels, args.max_height,
                          args.min_height, device=args.device).float()
-        seq = torch.randint(0, args.num_tokens, (batchsize,
-                                                 args.max_seq_len), device=args.device).long()
+        seq = torch.randint(0, args.num_tokens, (batchsize, args.max_seq_len), device=args.device).long()
         decoder(seq, context=encoder(im)).sum().backward()
         model.zero_grad()
         torch.cuda.empty_cache()
