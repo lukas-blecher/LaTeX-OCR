e30a5e0ed5c6457bfc112f3979047b30d25f2ebf
Lukas Blecher <luk.blecher@gmail.com>
2023-05-21 13:22:13 +0200

    Bump up version number

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ highlight = ['pygments']
 
 setuptools.setup(
     name='pix2tex',
-    version='0.1.1',
+    version='0.1.2',
     description='pix2tex: Using a ViT to convert images of equations into LaTeX code.',
     long_description=long_description,
     long_description_content_type='text/markdown',
