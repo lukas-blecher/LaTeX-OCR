7e94b309537546a1cc5e5a3ef04340fd08a1d3d7
JuanseHevia <jh216@rice.edu>
2024-03-23 16:06:18 -0500

    setup multilne model comparison in parallel

---
+++ multiprocess_eval_multiline.py
@@ -0,0 +1,59 @@
+import pickle
+import pix2tex as p2t
+from munch import Munch
+import yaml
+from pix2tex import cli as p2t
+from pix2tex.dataset.dataset import Im2LatexDataset
+import multiprocessing
+from metrics_facu import get_bleu_per_seq
+
+multiline_config = Munch({
+    "config": "../../multiline_checkpoints/local_multiline_pix2text/config.yaml",
+    "checkpoint": "../../multiline_checkpoints/local_multiline_pix2text/local_multiline_pix2text_e01_step99.pth",
+    "no_cuda": True,
+    "no_resize": False,
+    "device" : "cpu"
+})
+
+original_config = Munch({
+    "config": "settings/config.yaml",
+    "checkpoint": "model/checkpoints/weights.pth",
+    "no_cuda": True,
+    "no_resize": False,
+    "device": "cpu"
+}) 
+multiline_model = p2t.LatexOCR(multiline_config)
+original_model = p2t.LatexOCR()
+# load test set for handwritten files
+# load yaml files to parse configurations
+with open("pix2tex/model/settings/handwritten_training.yaml", 'r') as f:
+    hw_config_yaml = Munch(yaml.safe_load(f))
+
+with open("pix2tex/model/settings/config.yaml", 'r') as f:
+    original_config_yaml = Munch(yaml.safe_load(f))
+
+# test dataset
+test_dataset = Im2LatexDataset().load("pix2tex/dataset/multiline/test_dataset.pkl")
+
+
+def evaluate_and_pickle(model, dataset, config, fname):
+    """
+    Evaluate the model on the given dataset and store the results
+    """
+    results = get_bleu_per_seq(model, dataset, config)
+
+    with open("notebooks/multiline_performance/{}.pkl".format(fname), "wb") as f:
+        pickle.dump(results, f)
+    print("Results stored in {}.pkl".format(fname))
+
+
+if __name__ == "__main__":
+    # Compute multiprocessing
+    NUM_CORES = 2
+    pool = multiprocessing.Pool(NUM_CORES)
+    args = [(multiline_model.model, test_dataset, multiline_config, "MP-multiline_results"),
+             (original_model.model, test_dataset, original_config, "MP-original_results")]
+    pool.map(evaluate_and_pickle, args)
+
+    pool.close()
+    pool.join()
