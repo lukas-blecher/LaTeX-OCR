d3c68b7f7251f18de33d85a3f8353653c25cedf9
JuanseHevia <jh216@rice.edu>
2024-03-28 16:52:00 -0500

    added short test dataset. edited notebooks to compute new dimensions for dataset. performance metrics module.

--- metrics.py
+++ metrics.py
@@ -1,3 +1,4 @@
+import re
 from Levenshtein import distance
 import pix2tex.utils as p2t_utils
 import pix2tex.models as p2t_models
@@ -56,6 +57,25 @@ def get_model_and_data(config_path, checkpoint_path, data_path, batch_size=1, te
 
     return model, dataset, args
 
+def extraer_numero(texto):
+    """
+    Función para extraer el primer número encontrado en una cadena de texto.
+
+    Parámetros:
+    - texto (str): La cadena de texto de la cual extraer el número.
+
+    Retorna:
+    - str: El primer número encontrado en la cadena de texto.
+    - None: Si no se encuentra ningún número.
+    """
+    # Utilizar expresión regular para encontrar todos los números en la cadena
+    numeros = re.findall(r'\d+', texto)
+
+    # Asumiendo que solo hay un número en la cadena, obtener el primer resultado
+    numero = numeros[0] if numeros else None
+
+    return numeros
+
 
 def evaluate(model, dataset: Im2LatexDataset, args: Munch, num_batches: int = None):
     """evaluates the model. Returns bleu score on the dataset
@@ -74,16 +94,20 @@ def evaluate(model, dataset: Im2LatexDataset, args: Munch, num_batches: int = No
     device = args.device
     bleus, edit_dists, token_acc = [], [], []
     bleu_score, edit_distance, token_accuracy = 0, 1, 0
-    pbar = tqdm(enumerate(iter(dataset)), total=len(dataset))
+    iter_ds = iter(dataset)
+    pbar = tqdm(enumerate(iter_ds), total=len(dataset))
     preds = defaultdict(list)
+    pred_truth = defaultdict(list)
     for i, (seq, im) in pbar:
         if seq is None or im is None:
             continue
         #loss = decoder(tgt_seq, mask=tgt_mask, context=encoded)
         dec = model.generate(im.to(device), temperature=args.get('temperature', .2))
         pred = detokenize(dec, dataset.tokenizer)
+        tokenized_pred = token2str(dec, dataset.tokenizer)
         preds[i].append(pred)
         truth = detokenize(seq['input_ids'], dataset.tokenizer)
+        tokenized_truth = token2str(seq['input_ids'], dataset.tokenizer)
         bleus.append(metrics.bleu_score(pred, [alternatives(x) for x in truth]))
         for predi, truthi in zip(token2str(dec, dataset.tokenizer), token2str(seq['input_ids'], dataset.tokenizer)):
             ts = post_process(truthi)
@@ -100,6 +124,17 @@ def evaluate(model, dataset: Im2LatexDataset, args: Munch, num_batches: int = No
         tok_acc = (dec == tgt_seq)[mask].float().mean().item()
         token_acc.append(tok_acc)
         pbar.set_description('BLEU: %.3f, ED: %.2e, ACC: %.3f' % (np.mean(bleus), np.mean(edit_dists), np.mean(token_acc)))
+
+        #Busco el nombre de la imagen 
+        batch = iter_ds.pairs[iter_ds.i - 1]
+        _,ims=batch.T
+        label = extraer_numero(ims[0])[1]
+        pred_truth[label] = {'predicted': tokenized_pred,
+                             'truth':tokenized_truth,
+                             'pred_tokens':pred,
+                             'truth_tokens':truth,
+                             'bleu':bleu_score,
+                             'token acc':tok_acc}
         if num_batches is not None and i >= num_batches:
             break
     if len(bleus) > 0:
