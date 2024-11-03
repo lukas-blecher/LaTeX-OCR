f684cbbe209110bb2472501debb284ab8fd24a81
Oct0pu5 <86554778+Octopus058@users.noreply.github.com>
2024-11-03 23:04:02 +0800

    Fix resources.py

--- pix2tex/resources/resources.py
+++ pix2tex/resources/resources.py
@@ -3,7 +3,7 @@
 # Created by: The Resource Compiler for Qt version 6.4.2
 # WARNING! All changes made in this file will be lost!
 
-from PySide6 import QtCore
+from PyQt6 import QtCore
 
 qt_resource_data = b"\
 \x00\x02*\x89\
